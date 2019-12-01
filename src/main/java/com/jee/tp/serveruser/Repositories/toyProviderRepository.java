2c9147b24221b7372e67f9b5492ee939915f215b
ASHR3F1 <58385818+ASHR3F1@users.noreply.github.com>
2019-12-01 10:01:04 +0100

    Delete toyProviderRepository.java

--- src/main/java/com/jee/tp/serveruser/Repositories/toyProviderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jee.tp.serveruser.Repositories;
-
-import com.jee.tp.serveruser.Models.toyProvider;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface toyProviderRepository extends JpaRepository<toyProvider, Long> {
-}
