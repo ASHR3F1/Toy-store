0547234b2c62817908595a430dc8de0585c94a44
ASHR3F1 <58385818+ASHR3F1@users.noreply.github.com>
2019-12-01 10:01:34 +0100

    Delete customerLikesRepository.java

--- src/main/java/com/jee/tp/serveruser/Repositories/customerLikesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jee.tp.serveruser.Repositories;
-
-import com.jee.tp.serveruser.Models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface customerLikesRepository extends JpaRepository<Customer,Long>{
-
-}
